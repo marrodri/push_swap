68889d15f67890eb2d58e47698bd7742c50e3a75
marrodri <laza.mar64@gmail.com>
2019-09-19 15:45:18 -0700

    lstaddend implented in libft

--- libft/libft.h
+++ libft/libft.h
@@ -83,6 +83,7 @@ t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstadd(t_list **alst, t_list *new);
+void				ft_lstaddend(t_list **alst, t_list *new);
 t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 void				ft_print_bits(unsigned char octet);
